972c9e833c0fd70d73977d70d6a5065e941bfe88
aihubpro-ai <vasu081120@gmail.com>
2026-07-05 09:45:16 +0530

    Delete netlify directory

--- netlify/functions/get-key.js
+++
@@ -1,9 +0,0 @@
-exports.handler = async () => {
-  return {
-    statusCode: 200,
-    headers: { "Access-Control-Allow-Origin": "*" },
-    body: JSON.stringify({ 
-      key: process.env.RAZORPAY_KEY_ID 
-    })
-  }
-}
